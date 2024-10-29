5d76d809b501feca871ba9212769199061c4a2c0
Michal <none>
2024-10-29 11:26:57 +0100

    added class student

---
+++ student.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise_solution
+using System;
+
+
+public class Student
+{
+	public string name { get; private set; }
+	public int Grade 
+	{
+		get;
+		set {if(value>=1 && value <= 6){Grade = value;}}
+	}
+	public Student(string name, int grade)
+	{
+		this.name = name;
+		Grade = grade;
+	}
+	public void IsPassing()
+	{
+		int grade = int.parse(Console.ReadLine());
+		if (Grade > 2)
+		{
+			console.write($"Uczen {name} zdaje");
+			return 1;
+		}
+		else
+		{
+			Console.Write($"Uczen {name} nie zdaje")
+			return 0;
+		}
+	}
+}
