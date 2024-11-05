67d0c64c348779c5fdc0315bacedf1113c2c96ed
KacperCieslinski <kacper.cieslinski@uczen.zsk.poznan.pl>
2024-11-05 12:22:43 +0100

    plik 2 caly zrobiony

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -8,7 +8,7 @@ bmw.DisplayMaxSpeed(bmw.MaxSpeed);
 kawasaki.DisplayMaxSpeed(kawasaki.MaxSpeed);
 
 public class Vehicle(int maxspeed)
-{
+{   
     public int MaxSpeed = maxspeed;
 
     public void DisplayMaxSpeed(int maxspeed)
@@ -39,5 +39,35 @@ public class Cat(string race) : Animal(race)
     public override void MakeSound(string race)
     {
         Console.WriteLine($"{this.GetType()} makes meow meow"); ;
+    }
+}
+
+public class Employee(string firstname, string secondname,int basesalary)
+{
+    public string FirstName = firstname;    
+    public string SecondName = secondname;
+    public int BaseSalary = basesalary; 
+    public void CalculateSalary()
+    {
+        Console.WriteLine($"{this.BaseSalary}");
+    }
+}
+
+public class Manager(string firstname, string secondname, int basesalary, int bonus) : Employee(firstname, secondname, basesalary)
+{
+    public int Bonus = bonus;
+    public void CalculateSalary()
+    {
+        int salary = this.BaseSalary + this.Bonus;
+        Console.WriteLine(salary); 
+    }
+}
+public class SalesPerson(string firstname, string secondname, int basesalary, int commision, int salesvalue) : Employee(firstname, secondname, basesalary) { 
+    public int Commision = commision;
+    public int Salesvalue = salesvalue;
+    public void CalculateSalary()
+    {
+        int salary = this.BaseSalary + this.Commision * this.Salesvalue;
+        Console.WriteLine(salary);
     }
 }
