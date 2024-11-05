1963b6f26cd16b53190a07b87880af7b2cc24f09
KacperCieslinski <kacper.cieslinski@uczen.zsk.poznan.pl>
2024-11-05 12:16:14 +0100

    zad2

---
+++ OOP2/Program.cs
@@ -0,0 +1,43 @@
+﻿Car bmw = new Car(240);
+Motorcycle kawasaki = new Motorcycle(400);
+Dog Fido = new Dog("mieszaniec");
+Fido.MakeSound(Fido.Race);
+Cat ben = new Cat("brytyjski");
+ben.MakeSound(ben.Race);
+bmw.DisplayMaxSpeed(bmw.MaxSpeed);
+kawasaki.DisplayMaxSpeed(kawasaki.MaxSpeed);
+
+public class Vehicle(int maxspeed)
+{
+    public int MaxSpeed = maxspeed;
+
+    public void DisplayMaxSpeed(int maxspeed)
+    {
+        Console.WriteLine($"{this.GetType()} {maxspeed}");
+    }
+}
+
+public class Car(int maxspeed) : Vehicle(maxspeed);
+public class Motorcycle(int maxspeed):Vehicle(maxspeed);
+
+
+abstract public class Animal(string race)
+{
+    public string Race = race;
+    abstract public void MakeSound(string race);
+}
+
+public class Dog(string race) : Animal(race)
+{
+    public override void MakeSound(string race)
+    {
+        Console.WriteLine($"{this.GetType()} makes woof woof"); ;
+    }
+}
+public class Cat(string race) : Animal(race)
+{
+    public override void MakeSound(string race)
+    {
+        Console.WriteLine($"{this.GetType()} makes meow meow"); ;
+    }
+}
---
+++ inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿Human Ewok = new Human("Ewok", 19);
+Ewok.Eat("gowno");
+Student Ewoczek = new Student("Ewok", 69);
+Ewoczek.Learn("meth");
+Ewoczek.Eat("Marchewka");
+Employee Ejwon = new Employee("Ewok", 420);
+Ejwon.Work("Ksieżyc planety endor");
+class Human(string name, int age)
+{
+    public int Age = age;
+    public string Name = name;
+
+    public void Eat(string food)
+    {
+        Console.WriteLine($"{Name} eats: {food}");
+    }
+}
+class Employee(string name, int age) : Human(name, age)
+{
+    public void Work(string WorkPlace)
+    {
+        Console.WriteLine($"{name} works in: {WorkPlace}");
+    }
+}
+class Student(string name, int age) : Human(name, age)
+{
+    public int Age = age;
+    public string Name = name;
+    public void Learn(string subject)
+    {
+        Console.WriteLine($"{Name} learns: {subject}");
+    }
+}
